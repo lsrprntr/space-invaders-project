b54112a9fcb0e9405d2d496690c5065c5064ac48
lsr <lsrprntr@gmail.com>
2023-05-23 15:30:24 +1000

    back and forth draft

--- spaceinvaders.py
+++ spaceinvaders.py
@@ -30,11 +30,13 @@ class Rocket():
     
 
 class Game():
-    
+
     def __init__(self,width,height):
         #initializes the pygame engine
         pygame.init() 
         #screen size
+        self.width = width
+        self.height = height
         self.screen = pygame.display.set_mode((width, height))
         #details
         pygame.display.set_caption("Space Invaders")
@@ -44,21 +46,24 @@ class Game():
         self.clock = pygame.time.Clock()
         self.clock.tick(60)
         
-        
+        aliens = [] #aliens list will generate later
+
         #background = pygame.image.load('background.png') #background if needed, default black empty
-        
+        alien = Enemy(self,30,30)
+        aliens.append(alien)
+
         # define a variable to control the main loop
         running = True
         while running:
+            self.screen.fill((0, 0, 0)) #resets screen to blank
 
-        # event handling, gets all event from the event queue
+            # event handling, gets all event from the event queue
             for event in pygame.event.get():
                 print(event)
                 # only do something if the event is of type QUIT
                 if event.type == pygame.QUIT:
                     running = False
             
-            aliens = [Enemy(self,30,30)] #aliens list will generate later
             for alien in aliens:
                 alien.draw()
 
@@ -68,15 +73,28 @@ class Game():
 
 class Enemy(): 
     def __init__(self,game,x,y):
-        self.lives = 1
         self.x = x
         self.y = y
         self.game = game
+        self.direction = random.randint(0,1)
+        self.size = 20
     
     def draw(self):
         pygame.draw.rect(self.game.screen,
-                         (0,0,255),
+                         (0,255,0), #RGB color
                          pygame.Rect(self.x,self.y,20,20))
+        
+        if self.direction == 1:
+            self.x += 1
+        else:
+            self.x -= 1
+        
+        if self.x > self.game.width - self.size or self.x < 0:
+            if self.direction == 1:
+                self.direction = 0
+            else:
+                self.direction = 1
+
 
     
     def fire():
