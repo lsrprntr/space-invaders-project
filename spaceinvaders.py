a6c56dd802909d446ddc7b4ca9da242f768ba258
lsr <lsrprntr@gmail.com>
2023-05-23 14:59:21 +1000

    blipped a blue box on the screen

--- spaceinvaders.py
+++ spaceinvaders.py
@@ -2,13 +2,17 @@ import pygame
 import random
 import math
 
-class player():
+class Player():
     def __init__(self):
         self.lives=3
+        self.x = 0
+        self.y = 0
         return
     def moveleft(self):
+        self.x -= 1
         return
     def moveright(self):
+        self.x += 1
         return
     def lives():
         return
@@ -16,11 +20,65 @@ class player():
         return
     def fire():
         return
+    
+class Rocket():
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
 
-class enemy(player): 
-    def __init__(self):
+
+    
+
+class Game():
+    
+    def __init__(self,width,height):
+        #initializes the pygame engine
+        pygame.init() 
+        #screen size
+        self.screen = pygame.display.set_mode((width, height))
+        #details
+        pygame.display.set_caption("Space Invaders")
+        icon = pygame.image.load('images/ship.png')
+        pygame.display.set_icon(icon)
+        self.screen.fill((0,0,0))
+        self.clock = pygame.time.Clock()
+        self.clock.tick(60)
+        
+        
+        #background = pygame.image.load('background.png') #background if needed, default black empty
+        
+        # define a variable to control the main loop
+        running = True
+        while running:
+
+        # event handling, gets all event from the event queue
+            for event in pygame.event.get():
+                print(event)
+                # only do something if the event is of type QUIT
+                if event.type == pygame.QUIT:
+                    running = False
+            
+            aliens = [Enemy(self,30,30)] #aliens list will generate later
+            for alien in aliens:
+                alien.draw()
+
+            #update screen    
+            pygame.display.flip()
+
+
+class Enemy(): 
+    def __init__(self,game,x,y):
         self.lives = 1
-        return
+        self.x = x
+        self.y = y
+        self.game = game
+    
+    def draw(self):
+        pygame.draw.rect(self.game.screen,
+                         (0,0,255),
+                         pygame.Rect(self.x,self.y,20,20))
+
+    
     def fire():
         return
     def death():
@@ -29,32 +87,6 @@ class enemy(player):
         return
     
 
-class game():
-    
-    def __init__(self,width,height):
-        #initializes the pygame engine
-        pygame.init() 
-        #screen size
-        self.screen = pygame.display.set_mode((width, height))
-
-        #details
-        pygame.display.set_caption("Space Invaders")
-        #icon = pygame.image.load('ufo.png')
-        #pygame.display.set_icon(icon)
-        #background = pygame.image.load('background.png') #background if needed, default black empty
-
-
-        self.clock = pygame.time.Clock()
-
-        # define a variable to control the main loop
-        running = True
-        while running:
-        # event handling, gets all event from the event queue
-            for event in pygame.event.get():
-                # only do something if the event is of type QUIT
-                if event.type == pygame.QUIT:
-                    running = False
-        
-
-
-start = game(800,600)
+if __name__ == '__main__':
+    game = Game(800, 600)
+#start = Game(800,600)
