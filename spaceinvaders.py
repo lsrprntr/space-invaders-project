31bf31809b49dd15efbc1dacada124a368d76e09
lsr <lsrprntr@gmail.com>
2023-05-23 16:31:44 +1000

    key hold troubleshooting

--- spaceinvaders.py
+++ spaceinvaders.py
@@ -3,30 +3,23 @@ import random
 import math
 
 class Player():
-    def __init__(self):
+    def __init__(self,game):
+        self.game = game
         self.lives=3
-        self.x = 0
-        self.y = 0
-        return
-    def moveleft(self):
-        self.x -= 1
-        return
-    def moveright(self):
-        self.x += 1
-        return
-    def lives():
-        return
-    def checkhit():
-        return
-    def fire():
-        return
+        self.x = self.game.width/2 - 13
+        self.y = self.game.height - 25
+
+    def draw(self):
+        pygame.draw.rect(self.game.screen,
+                         (0, 0, 255),
+                         pygame.Rect(self.x, self.y, 25, 25))
+        
+
     
 class Rocket():
     def __init__(self,x,y):
         self.x = x
         self.y = y
-
-
     
 
 class Game():
@@ -42,30 +35,50 @@ class Game():
         pygame.display.set_caption("Space Invaders")
         icon = pygame.image.load('images/ship.png')
         pygame.display.set_icon(icon)
+        #background = pygame.image.load('background.png') #background if needed, default black empty
+
         self.screen.fill((0,0,0))
         self.clock = pygame.time.Clock()
         self.clock.tick(60)
         
         aliens = [] #aliens list will generate later
-
-        #background = pygame.image.load('background.png') #background if needed, default black empty
         alien = Enemy(self,30,30)
         aliens.append(alien)
+
+        players = []
+        player = Player(self)
+        players.append(player)
 
         # define a variable to control the main loop
         running = True
         while running:
             self.screen.fill((0, 0, 0)) #resets screen to blank
-
+            pressed = pygame.key.get_pressed()
             # event handling, gets all event from the event queue
             for event in pygame.event.get():
                 print(event)
+                reset = 1
                 # only do something if the event is of type QUIT
                 if event.type == pygame.QUIT:
                     running = False
-            
+
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_RIGHT and reset == 1:
+                        player.x += 5
+                        print(player.x)
+                    elif event.key == pygame.K_LEFT and reset == 1:
+                        player.x -= 5
+                    reset = 0
+
+
+            #aliens draw loop
             for alien in aliens:
                 alien.draw()
+            
+            #player draw loop
+            for ship in players:
+                ship.draw()
+                
 
             #update screen    
             pygame.display.flip()
